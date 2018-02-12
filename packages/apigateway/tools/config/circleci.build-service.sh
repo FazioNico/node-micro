59bf9560532e63628902d010dedab12ab889e33e
FazioNico <contact@nicolasfazio.ch>
2018-02-12 21:22:14 +0100

    .

--- packages/apigateway/tools/config/circleci.build-service.sh
+++ packages/apigateway/tools/config/circleci.build-service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yarn install --production --silent --non-interactive --no-lockfile
+yarn install --silent --non-interactive --no-lockfile
 # build production
 yarn run webpack:prod
 # copy dist/ folder to .workflows cache directory
