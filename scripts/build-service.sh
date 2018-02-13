d1ac74d9095cc50a004f8972dc62856b477dad80
FazioNico <contact@nicolasfazio.ch>
2018-02-13 18:40:22 +0100

    .

--- scripts/build-service.sh
+++ scripts/build-service.sh
@@ -21,7 +21,6 @@ rootDirectory=$(pwd)
   # build docker image
   docker build -t registry.agenda.ch/fazio/$serviceName:$TAG  .
   # login to docker hub
-  echo "${USER_DOCKER}  -  ${PASS_DOCKER}"
   docker login registry.agenda.ch -u ${USER_DOCKER} -p ${PASS_DOCKER}
   # push docker image
   docker push registry.agenda.ch/fazio/$serviceName:$TAG
