a19e7b0836953602e9c18495cf1132e6c701c08c
FazioNico <contact@nicolasfazio.ch>
2018-02-13 09:50:12 +0100

    .

--- packages/users-service/tools/config/circleci.build-service.sh
+++ packages/users-service/tools/config/circleci.build-service.sh
@@ -4,6 +4,6 @@ yarn install --silent --non-interactive --no-lockfile
 # build production
 yarn run webpack:prod
 # copy dist/ folder to .workflows cache directory
-serviceName=$(pwd | sed 's#.*/##')
-mkdir -p ~/repo/docker-cache
-tar -cvf ~/repo/docker-cache/${serviceName}-build.tar ./platforms/
+# serviceName=$(pwd | sed 's#.*/##')
+# mkdir -p ~/repo/docker-cache
+# tar -cvf ~/repo/docker-cache/${serviceName}-build.tar ./platforms/
