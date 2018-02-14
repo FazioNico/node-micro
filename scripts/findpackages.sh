bd3137f97c4b8ccae30294c71d289010238821b4
FazioNico <contact@nicolasfazio.ch>
2018-02-14 11:43:56 +0100

    ..

--- scripts/findpackages.sh
+++ scripts/findpackages.sh
@@ -6,13 +6,13 @@ latestTag=$(git tag | sort -r | head -1)
 latestCmt=$(git log -1 --format="%H")
 
 SHA1=fddfbe0 # older
-SHA2=8527e51 # newer
+SHA2=22480e6 # newer
 
-packages=$(git diff --name-only ${SHA1} ${SHA2} -- packages  | awk '{ split($0,a,/\//); print a[1]"/"a[2] }' | uniq)
+packages=$(git diff --name-only ${SHA1} ${SHA2} -- packages  | awk '{ split($0,a,/\//); print a[1]"/"a[2] }' | uniq )
 
-echo $packages
-# for dir in $packages; do
-#   echo $dir
-# done
+# echo $packages
+for dir in $packages; do
+  echo $dir | sed 's#.*/##'
+done
 
 #
