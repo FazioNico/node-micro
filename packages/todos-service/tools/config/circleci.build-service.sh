5845ce51fb6bc7a6239331dd63a9ed7f1c11a5b9
FazioNico <contact@nicolasfazio.ch>
2018-02-14 15:20:13 +0100

    Upd: Fix dev and build script

--- packages/todos-service/tools/config/circleci.build-service.sh
+++ packages/todos-service/tools/config/circleci.build-service.sh
@@ -2,8 +2,8 @@
 
 yarn install --silent --non-interactive --no-lockfile
 # build production
-yarn run build:server
-# copy dist/ folder to .workflows cache directory
-serviceName=$(pwd | sed 's#.*/##')
-mkdir -p ~/repo/docker-cache/${serviceName}/platforms/server
-cp -r ./platforms/server ~repo/docker-cache/${serviceName}/platforms/server
+yarn run webpack:prod
+# # copy dist/ folder to .workflows cache directory
+# serviceName=$(pwd | sed 's#.*/##')
+# mkdir -p ~/repo/docker-cache/${serviceName}/platforms/server
+# cp -r ./platforms/server ~repo/docker-cache/${serviceName}/platforms/server
