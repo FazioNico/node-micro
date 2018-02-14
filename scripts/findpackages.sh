f4907fcf4db4f583594343ae5b6a6cda7691e850
webmaster-fazio <webmaster-fazio@MacBook-Pro-de-Fazio.local>
2018-02-14 12:08:46 +0100

    build#

--- scripts/findpackages.sh
+++ scripts/findpackages.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # find packages changed
+function get_last_tag_hash {
+  tag=$(git tag | sort -r | head -1)
+  if [ -n "$tag" ]; then
+    git rev-list $tag -n1
+  fi
+}
 
-latestTag=$(git tag | sort -r | head -1)
+latestTag=$(get_last_tag_hash)
 latestCmt=$(git log -1 --format="%H")
 
-SHA1=fddfbe0 # older
-SHA2=22480e6 # newer
 
-packages=$(git diff --name-only ${SHA1} ${SHA2} -- packages  | awk '{ split($0,a,/\//); print a[1]"/"a[2] }' | uniq )
+packages=$(git diff --name-only ${latestTag} ${latestCmt} -- packages  | awk '{ split($0,a,/\//); print a[1]"/"a[2] }' | uniq )
 
-# echo $packages
-for dir in $packages; do
-  echo $dir | sed 's#.*/##'
-done
+# # echo $packages
+# for dir in $packages; do
+#   echo $dir | sed 's#.*/##'
+# done
 
 #
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,28 @@
-# defin project rootDirectory
-rootDirectory=$(pwd)
-mkdir docker-cache
-# do loop for each microservice found into packages folder
-#for dir in $(ls -d  packages/*); do
-  # go to microservice folder
-  # cd $dir
-  cd packages/users-service
-  serviceName=$(pwd | sed 's#.*/##')
+#!/bin/bash
 
-  echo "[INSTALL] ${serviceName} microservice: packages dependencies"
-  # install project dependencies
-  docker build -f Dockerfile.dev -t ${serviceName} .
-
-  docker save -o $rootDirectory/docker-cache/${serviceName}.tar ${serviceName}
-
-  # bash ./tools/config/circleci.install.sh
-  # return to rootDirectory project
-  cd $rootDirectory
-# done
-
-echo '----------------------------------------------------'
-echo '----------------------------------------------------'
-echo '[SUCCESS] Project dependencies install with success!'
+source $(pwd)/scripts/release.sh
+echo  $packages
+# # defin project rootDirectory
+# rootDirectory=$(pwd)
+# mkdir docker-cache
+# # do loop for each microservice found into packages folder
+# #for dir in $(ls -d  packages/*); do
+#   # go to microservice folder
+#   # cd $dir
+#   cd packages/users-service
+#   serviceName=$(pwd | sed 's#.*/##')
+#
+#   echo "[INSTALL] ${serviceName} microservice: packages dependencies"
+#   # install project dependencies
+#   docker build -f Dockerfile.dev -t ${serviceName} .
+#
+#   docker save -o $rootDirectory/docker-cache/${serviceName}.tar ${serviceName}
+#
+#   # bash ./tools/config/circleci.install.sh
+#   # return to rootDirectory project
+#   cd $rootDirectory
+# # done
+#
+# echo '----------------------------------------------------'
+# echo '----------------------------------------------------'
+# echo '[SUCCESS] Project dependencies install with success!'
